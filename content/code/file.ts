f1f3972e3b9d513670cb32654dc164e470c2af85
Jitse De Smet <jitse@reedonk8.be>
2025-04-17 17:17:06 +0200

    write about lexerbuilder and parserbuilder

---
+++ content/code/file.ts
@@ -0,0 +1,10 @@
+export const iriOrNil: SparqlRule<'iriOrNil', URL | null> = <const>{
+  name: 'iriOrNil',
+  impl: ({SUBRULE, CONSUME, OR}) => () => OR<URL | null>([
+    {ALT: () => SUBRULE(iri, undefined)},
+    {ALT: () => {
+        CONSUME(l.terminals.nil);
+        return null;
+      } },
+  ]),
+};
